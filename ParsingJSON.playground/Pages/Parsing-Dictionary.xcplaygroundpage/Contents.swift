d11c6841dc341ed5867a23ec82100f87b79abca5
CameronRivera <cameronrivera@pursuit.org>
2020-07-28 12:24:13 -0400

    Added example for JSON dictionary parsing.

--- ParsingJSON.playground/Pages/Parsing-Dictionary.xcplaygroundpage/Contents.swift
+++ ParsingJSON.playground/Pages/Parsing-Dictionary.xcplaygroundpage/Contents.swift
@@ -2,4 +2,47 @@
 
 import Foundation
 
+// JSON Data
+let json = """
+{
+ "results": [
+   {
+     "firstName": "John",
+     "lastName": "Appleseed"
+   },
+  {
+    "firstName": "Alex",
+    "lastName": "Paul"
+  }
+ ]
+}
+""".data(using: .utf8)! // Turns this json into data.
+//=======================================================
+// Create Model(s)
+//=======================================================
 
+// Coadable: An alias for Coadable & Decodable
+// Decodable: Converts json data into Swift types.
+// Encodable: Changes swift data types into JSON data.
+
+// Top Level JSON is a Dictionary
+struct ResultsWrapper: Decodable {
+    let results: [Contact]
+}
+
+struct Contact: Decodable {
+    let firstName: String
+    let lastName: String
+}
+
+//=======================================================
+// Decode the JSON data to our Swift Model
+//=======================================================
+
+do {
+    let dictionary = try JSONDecoder().decode(ResultsWrapper.self, from: json)
+    let contacts = dictionary.results // [Contact]
+    dump(contacts)
+} catch {
+    print("Decoding Error: \(error.localizedDescription)")
+}
